4b0d99e1dae506274535e42f52092532c2086a27
Y Duong Ngoc <ydn@hoya.com>
2025-07-23 15:03:32 +0700

    Tu dong goi request 1

--- request-dinh-ky.js
+++ request-dinh-ky.js
@@ -1,29 +1,19 @@
 const express = require('express');
-const cors = require('cors'); // Import thư viện cors
-const { Pool } = require('pg');
+const cors = require('cors');
 const cron = require('node-cron');
+const fetch = require('node-fetch'); // hoặc axios
 
 const app = express();
 app.use(cors()); // Sử dụng middleware CORS
 app.use(express.json());
 
-// Cấu hình kết nối đến PostgreSQL
-const pool = new Pool({
-    user: 'postgres', // Thay thế bằng username của bạn
-    host: '14.165.172.59',//địa chỉ ip công khai của máy fujitsu 
-    database: 'Invoice',
-    password: '1!Ngaycuoicung', // Thay thế bằng password của bạn
-    port: 5432,
-});
 
-cron.schedule('*/5 * * * *', async () => {
-  console.log('[PING] Giữ database luôn thức 😴➡️😎');
-  try {
-    const result = await pool.query('SELECT 1');
-    console.log(`[PING] OK at ${new Date().toISOString()}`);
-  } catch (err) {
-    console.error('[PING] Fail:', err.message);
-  }
+cron.schedule('* * * * *', () => {
+  console.log('Đang gửi request giữ tỉnh táo 😴');
+  fetch('https://api-create-new-user.onrender.com/Invoice?username=toan&kieuyeucau=checkusertontai')
+    .then(res => res.text())
+    .then(data => console.log('Đã gửi xong:', data))
+    .catch(err => console.error('Lỗi gửi request:', err));
 });
 
 // Khởi động server
