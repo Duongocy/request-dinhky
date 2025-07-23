d0f1032c6a005a4b9951f86668ac3f42a19ceb4e
Dương Ngọc Ý <duongocy@gmail.com>
2025-07-23 17:25:48 +0700

    goi request dinh ky 4

--- request-dinh-ky.js
+++ request-dinh-ky.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const cron = require('node-cron');
-const fetch = require('node-fetch'); // hoặc axios
 const https = require('https');
 
 const app = express();
